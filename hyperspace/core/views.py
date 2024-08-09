c9993cdc60b0cf2461a221099c79df46698160a0
gurcancekic <arinyazilim@gmail.com>
2024-08-10 01:04:24 +0300

    Lesson 04

--- hyperspace/core/views.py
+++ hyperspace/core/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Welcome to the Hyperspace Django Project!")
+    return render(request, 'core/index.html')
