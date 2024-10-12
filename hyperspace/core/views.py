fcd76c16c80ec5a154164668bf08ea413b88710a
gurcancekic <arinyazilim@gmail.com>
2024-10-12 19:15:43 +0300

    Lesson 15

--- hyperspace/core/views.py
+++ hyperspace/core/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+from django.conf import settings
+from django.urls import reverse
+from django.contrib import messages
 from services.models import Service
 from blog.models import Blog
 
@@ -20,3 +25,25 @@ def index(request):
     context = {'static_content': static_content, 'featured_services': featured_services, 'latest_posts': latest_posts}
 
     return render(request, 'core/index.html', context)
+
+def contact(request):
+    if request.method == 'POST':
+
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        # Compose the message
+        subject = f"New contact form submission from {name}"
+        message_body = f"Message from {name} ({email}):\n\n{message}"
+        recipient_list = [settings.EMAIL_HOST_USER]  # Your email
+
+        # Send email to admin
+        try:
+            send_mail(subject, message_body, email, recipient_list)
+        except Exception as err:
+            messages.error(request, "Something happened, please try again later")
+        else:
+            messages.success(request, "We have received your email successfully!")
+        
+    return HttpResponseRedirect(reverse('index') + '#three')
